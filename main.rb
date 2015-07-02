4f3a1f84f87fd897f883106aaeebee516d0d94ce
Juan Castillo <castillobgr@gmail.com>
2015-07-02 09:17:50 -0500

    Enhancing UX.

--- main.rb
+++ main.rb
@@ -1,8 +1,13 @@
 require './event_handler'
 require './event_listener'
 
+PROMPT = '> '
+
 def run
   handler = EventHandler.new
+  puts 'Welcome to MegaObserver 9000. (c) Compuworld Hypermeganet.'
+  puts "Type '-h' for help."
+  print PROMPT
   while command = gets.chomp
     if command.empty?
       break
@@ -17,10 +22,23 @@ def run
       print 'Enter a message to send: '
       message = gets.chomp
       handler.send_event(message)
+    when '-h'
+      puts 'Available commands:'
+      puts " add\tAdd a new listener."
+      puts " send\tSend a new message."
+      puts "..."
+      puts "That's pretty much it."
     else
       puts "Couldn't understand that command"
     end
+
+    print PROMPT
   end
+end
+
+trap "SIGINT" do
+  puts "\nSo long, see you tomorrow."
+  exit 130
 end
 
 run
